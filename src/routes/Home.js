2108f8bff1540d38e833fee2c9be0cd6ef4e47b9
gksehfapd <1ehfapd@gmail.com>
2023-03-30 19:52:16 +0900

    style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Movie from '../components/Movie'
+import styles from './Home.module.css'
 
 function Home() {
 	const [loading, setLoading] = useState(true)
@@ -11,21 +12,22 @@ function Home() {
 		setMovies(json.data.movies)
 		setLoading(false)
 	}
-
 	useEffect(() => {
 		getMovies()
 	}, [])
-
 	return (
-		<div>
+		<div className={styles.container}>
 			{loading ? (
-				<h1>loading...</h1>
+				<div className={styles.loader}>
+					<span>Loading...</span>
+				</div>
 			) : (
-				<div>
+				<div className={styles.movies}>
 					{movies.map((movie) => (
 						<Movie
 							key={movie.id}
 							id={movie.id}
+							year={movie.year}
 							coverImg={movie.medium_cover_image}
 							title={movie.title}
 							summary={movie.summary}
@@ -37,5 +39,4 @@ function Home() {
 		</div>
 	)
 }
-
 export default Home
